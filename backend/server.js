1acd9b11736add661fa5b0c723742949ba3c6957
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-12 18:35:50 +0200

    server configuration/combination

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
-import express from "express";
+const express = require("express");
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("This is from backend!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT: ${PORT}`);
+});
