ff6615420a0dcb103a62d1772195ac3c85bd0ff9
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-12 19:05:36 +0200

    routing through pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/messenger/login" element={<Login />} />
+          <Route path="/messenger/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
