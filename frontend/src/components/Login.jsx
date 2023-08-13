bfb7eaa2b4148882a2a456d44df04645f4bcac46
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-13 13:28:22 +0200

    Login page design

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="register">
+      <div className="card">
+        <div className="card-header">
+          <h3>Login</h3>
+        </div>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                placeholder="Email*"
+                id="email"
+                className="form-control"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password*"
+                id="password"
+                className="form-control"
+              />
+            </div>
+
+            <div className="form-group">
+              <input type="submit" value="login" className="btn" />
+            </div>
+
+            <div className="form-group">
+              <span>
+                <Link to="/messenger/register">Don't have an Account?</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
