7e90fc14516cb8bf8c1f1cc503fd6fd177c9ab6c
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-13 15:05:15 +0200

    posting data to the backend

--- frontend/src/store/actions/authAction.js
+++ frontend/src/store/actions/authAction.js
@@ -1,3 +1,22 @@
+import axios from "axios";
+
 export const userRegisterDispatch = (data) => {
-  return async (dispatch) => {};
+  return async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        "/api/messenger/user-register",
+        data,
+        config
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
 };
