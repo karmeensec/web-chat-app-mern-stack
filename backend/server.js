f0011c131438e1d29e9933c257f598e353ce2752
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-13 19:33:06 +0200

    configuring successful database connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+const dbConnect = require("./config/database");
+
+dotenv.config({ path: "backend/config/config.env" });
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("This is from backend!");
 });
+
+dbConnect();
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT: ${PORT}`);
