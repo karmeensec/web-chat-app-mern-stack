a3819989a33cb01f23f75bf5893872c30804e480
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-14 20:02:19 +0200

    parsing test data from frontend

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,3 +1,12 @@
+const formidable = require("formidable");
+
 module.exports.userRegister = (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, async (error, field, files) => {
+    console.log("Fields: ", field);
+    console.log("Files: ", files);
+  });
+
   console.log("Register is called");
 };
