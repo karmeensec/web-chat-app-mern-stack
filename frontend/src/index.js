d0bbb2b8e5de622a58089b10f35d219817db3c98
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-25 17:59:48 +0200

    wrapping app component to the alert

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,20 @@ import App from "./App";
 import "./main.scss";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import alertTemplate from "react-alert-template-basic";
+
+const options = {
+  timeout: 5000,
+  positions: positions.BOTTOM_CENTER,
+  transitions: transitions.SCALE,
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={alertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </Provider>
 );
