79147c445fce98c8caa4e06f09187a11e4d2dd17
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-25 15:48:16 +0200

    sending multipart form data to the backend

--- frontend/src/store/actions/authAction.js
+++ frontend/src/store/actions/authAction.js
@@ -6,7 +6,7 @@ export const userRegisterDispatch = (data) => {
       headers: {
         accept: "application/json",
         "Accept-Language": "en-US,en;q=0.8",
-        "Content-Type": `multipart/form-data;`,
+        "Content-Type": `multipart/form-data`,
       },
     };
 
