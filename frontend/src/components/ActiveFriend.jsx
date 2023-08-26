b4eadf9279a44fdb832c71fd1a93ef0bb4daf8a4
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-26 04:16:56 +0200

    rendering active friends

---
+++ frontend/src/components/ActiveFriend.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ActiveFriend = () => {
+  return (
+    <div className="active-friend">
+      <div className="image-active-icon">
+        <div className="image">
+          <img src="" alt="" />
+
+          <div className="active-icon"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActiveFriend;
