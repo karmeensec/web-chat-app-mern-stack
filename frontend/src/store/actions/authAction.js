fbfbfe496f54c792c1ade58294a8e41f4fc7e5cb
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-26 02:09:34 +0200

    login dispatch

--- frontend/src/store/actions/authAction.js
+++ frontend/src/store/actions/authAction.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { REGISTER_FAIL, REGISTER_SUCCESS } from "../types/authTypes";
+import {
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+} from "../types/authTypes";
 
 export const userRegisterDispatch = (data) => {
   return async (dispatch) => {
@@ -30,6 +35,41 @@ export const userRegisterDispatch = (data) => {
     } catch (error) {
       dispatch({
         type: REGISTER_FAIL,
+        payload: { error: error.response.data.error.errorMessage },
+      });
+    }
+  };
+};
+
+export const userLoginDispatch = (data) => {
+  return async (dispatch) => {
+    const config = {
+      headers: {
+        accept: "application/json",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        "/api/messenger/user-login",
+        data,
+        config
+      );
+
+      localStorage.setItem("authToken", response.data.token);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: {
+          successMessage: response.data.successMessage,
+          token: response.data.token,
+        },
+      });
+    } catch (error) {
+      dispatch({
+        type: LOGIN_FAIL,
         payload: { error: error.response.data.error.errorMessage },
       });
     }
