b16e2bbb843fa9c9b6c3655b994c904965298425
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-26 02:44:57 +0200

    creating initial messenger ui

---
+++ frontend/src/components/Messenger.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaEllipsis, FaRegPenToSquare, FaSistrix } from "react-icons/fa6";
+
+const Messenger = () => {
+  return (
+    <div className="messenger">
+      <div className="row">
+        <div className="col-3">
+          <div className="left-side">
+            <div className="top">
+              <div className="image-name">
+                <div className="image">
+                  <img src="./images/" alt="" />
+                </div>
+
+                <div className="name">
+                  <h3>Hi Me</h3>
+                </div>
+              </div>
+
+              <div className="icons">
+                <div className="icon">
+                  <FaEllipsis />
+                </div>
+
+                <div className="icon">
+                  <FaRegPenToSquare />
+                </div>
+              </div>
+            </div>
+
+            <div className="friend-search">
+              <div className="search">
+                <button>
+                  <FaSistrix />
+                </button>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="form-control"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Messenger;
