7b53553a157d1cc291f65537d9625199c753cd4f
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-26 04:17:07 +0200

    rendering friends

---
+++ frontend/src/components/Friends.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Friends = () => {
+  return (
+    <div className="friend">
+      <div className="friend-image">
+        <div className="image">
+          <img src="" alt="" />
+        </div>
+      </div>
+
+      <div className="friend-name-seen">
+        <div className="friend-name">
+          <h4>Kamil Isma</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Friends;
