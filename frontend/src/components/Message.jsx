fe506298ccb034aa5faa9cecff8cc3f937e8d2ee
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-27 13:24:58 +0200

    Message component

---
+++ frontend/src/components/Message.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="message-show">
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text"> Hi Me, How are you?</p>
+          </div>
+        </div>
+
+        <div className="time">27 August 2023</div>
+      </div>
+
+      <div className="friend-message">
+        <div className="image-message-time">
+          <img src="" alt="" />
+
+          <div className="message-time">
+            <div className="friend-text">
+              <p className="message-text">I am good</p>
+            </div>
+
+            <div className="time">26 August 2023</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
