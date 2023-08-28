2a200f6b3365ba98d213a67497c27ce273574b73
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-28 16:13:39 +0200

    diapatching he friend payload

--- frontend/src/store/actions/messengerAction.js
+++ frontend/src/store/actions/messengerAction.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { GET_FRIEND_SUCCESS } from "../types/messengerType";
 
 export const getUserFriends = () => {
   return async (dispatch) => {
@@ -7,6 +8,13 @@ export const getUserFriends = () => {
     try {
       const response = await axios.get("/api/messenger/get-friends");
       console.log("Response data is:", response.data);
+
+      dispatch({
+        type: GET_FRIEND_SUCCESS,
+        payload: {
+          friends: response.data.friends,
+        },
+      });
     } catch (error) {
       console.log("Error response data is: ", error.response.data);
     }
