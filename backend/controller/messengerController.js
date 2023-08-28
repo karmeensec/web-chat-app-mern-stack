1d4bc3afd0e9f266d5a5c75b211c80ff78e20c3a
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-28 17:57:04 +0200

    filtering the friends datas

--- backend/controller/messengerController.js
+++ backend/controller/messengerController.js
@@ -3,13 +3,20 @@ const userSchema = require("../models/authModel");
 module.exports.userGetFriends = async (req, res) => {
   console.log("User get friends called");
 
+  const userId = req.userId;
+  console.log("User Id: ", userId);
+
   try {
     const friendData = await userSchema.find({});
     console.log("Friends from database: ", friendData);
 
+    const filteredFriendData = friendData.filter(
+      (friend) => friend.id !== userId
+    );
+
     res.status(200).json({
       success: true,
-      friends: friendData,
+      friends: filteredFriendData,
     });
   } catch (error) {
     res.status(500).json({
