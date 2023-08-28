3229e4db956b9ce3544f513ff24000320a5ee6ee
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-28 16:01:00 +0200

    getting friend data from database to console

--- backend/controller/messengerController.js
+++ backend/controller/messengerController.js
@@ -1,3 +1,21 @@
+const userSchema = require("../models/authModel");
+
 module.exports.userGetFriends = async (req, res) => {
   console.log("User get friends called");
+
+  try {
+    const friendData = await userSchema.find({});
+    console.log("Friends from database: ", friendData);
+
+    res.status(200).json({
+      success: true,
+      friends: friendData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: {
+        errorMessage: "Internal Server Error",
+      },
+    });
+  }
 };
