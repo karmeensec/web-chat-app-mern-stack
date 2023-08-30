9b26a2597c08f3213e439c53a599e4b09bbc5f10
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-30 17:14:34 +0200

    creating message schema

---
+++ backend/models/messageModel.js
@@ -0,0 +1,40 @@
+const { model, Schema } = require("mongoose");
+
+const messageSchema = new Schema(
+  {
+    senderId: {
+      type: String,
+      required: true,
+    },
+
+    senderName: {
+      type: String,
+      required: true,
+    },
+
+    receiverId: {
+      type: String,
+      required: true,
+    },
+
+    message: {
+      text: {
+        type: String,
+        default: "",
+      },
+
+      image: {
+        type: String,
+        default: "",
+      },
+    },
+
+    staus: {
+      type: String,
+      default: "unseen",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("message", messageSchema);
