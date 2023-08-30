55414729aa87035bbf9e3861ec33e19e3ffd2047
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-08-30 17:29:27 +0200

    functionality for sending message

--- backend/controller/messengerController.js
+++ backend/controller/messengerController.js
@@ -1,4 +1,5 @@
 const userSchema = require("../models/authModel");
+const messageSchema = require("../models/messageModel");
 
 module.exports.userGetFriends = async (req, res) => {
   console.log("User get friends called");
@@ -31,4 +32,31 @@ module.exports.userSendMessage = async (req, res) => {
   console.log("User Send Message called");
 
   console.log("userSendMessage Req body: ", req.body);
+
+  const { senderName, receiverId, message } = req.body;
+
+  const senderId = req.userId;
+
+  try {
+    const addMessage = await messageSchema.create({
+      senderId: senderId,
+      senderName: senderName,
+      receiverId: receiverId,
+      message: {
+        text: message,
+        image: "",
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: addMessage,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: {
+        errorMessage: "Internal Server Error",
+      },
+    });
+  }
 };
