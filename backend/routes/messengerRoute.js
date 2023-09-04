b83e607ef7590414018b6eee1c346bd6be7ac697
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-04 21:07:22 +0200

    adding image message send route

--- backend/routes/messengerRoute.js
+++ backend/routes/messengerRoute.js
@@ -4,11 +4,13 @@ const {
   userGetFriends,
   userSendMessage,
   userGetMessage,
+  userSendImageMessage,
 } = require("../controller/messengerController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 router.get("/get-friends", authMiddleware, userGetFriends);
 router.post("/send-message", authMiddleware, userSendMessage);
 router.get("/get-message/:id", authMiddleware, userGetMessage);
+router.post("/image-message-send", authMiddleware, userSendImageMessage);
 
 module.exports = router;
