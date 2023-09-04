33e2e9737273e3bbc4ff1d9700444e2ce4f1236b
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-04 17:22:56 +0200

    get user message controller function

--- backend/controller/messengerController.js
+++ backend/controller/messengerController.js
@@ -60,3 +60,37 @@ module.exports.userSendMessage = async (req, res) => {
     });
   }
 };
+
+module.exports.userGetMessage = async (req, res) => {
+  console.log("User Get Message called");
+
+  console.log("userGetMessage Req params: ", req.params.id);
+
+  const userId = req.userId;
+  console.log("User Id: ", userId);
+
+  const friendId = req.params.id;
+
+  try {
+    let allMessages = await messageSchema.find({});
+
+    allMessages = allMessages.filter(
+      (message) =>
+        (message.senderId === userId && message.receiverId === friendId) ||
+        (message.receiverId === userId && message.senderId === friendId)
+    );
+
+    console.log("All Messages: ", allMessages);
+
+    res.status(200).json({
+      success: true,
+      message: allMessages,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: {
+        errorMessage: "Internal Server Error",
+      },
+    });
+  }
+};
