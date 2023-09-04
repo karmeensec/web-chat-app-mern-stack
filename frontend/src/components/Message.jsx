8d993756e00cf86beb14913ce7ce6c7178b32b60
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-04 18:33:14 +0200

    map and render messages

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,31 +1,40 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-const Message = ({ currentFriend }) => {
+const Message = ({ currentFriend, message }) => {
+  const { userInfo } = useSelector((state) => state.auth);
+
   return (
     <div className="message-show">
-      <div className="my-message">
-        <div className="image-message">
-          <div className="my-text">
-            <p className="message-text"> Hi Me, How are you?</p>
-          </div>
-        </div>
+      {message && message.length > 0
+        ? message.map((m) =>
+            m.senderId === userInfo.id ? (
+              <div className="my-message">
+                <div className="image-message">
+                  <div className="my-text">
+                    <p className="message-text"> {m.message.text} </p>
+                  </div>
+                </div>
 
-        <div className="time">27 August 2023</div>
-      </div>
+                <div className="time">27 August 2023</div>
+              </div>
+            ) : (
+              <div className="friend-message">
+                <div className="image-message-time">
+                  <img src={`./images/${currentFriend.image}`} alt="" />
 
-      <div className="friend-message">
-        <div className="image-message-time">
-          <img src={`./images/${currentFriend.image}`} alt="" />
+                  <div className="message-time">
+                    <div className="friend-text">
+                      <p className="message-text">{m.message.text}</p>
+                    </div>
 
-          <div className="message-time">
-            <div className="friend-text">
-              <p className="message-text">I am good</p>
-            </div>
-
-            <div className="time">26 August 2023</div>
-          </div>
-        </div>
-      </div>
+                    <div className="time">26 August 2023</div>
+                  </div>
+                </div>
+              </div>
+            )
+          )
+        : ""}
     </div>
   );
 };
