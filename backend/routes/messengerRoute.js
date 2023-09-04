e4ebc1820d34124af1099074d2a8d039c60853f7
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-04 17:22:37 +0200

    creating get message with id route

--- backend/routes/messengerRoute.js
+++ backend/routes/messengerRoute.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const {
   userGetFriends,
   userSendMessage,
+  userGetMessage,
 } = require("../controller/messengerController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 router.get("/get-friends", authMiddleware, userGetFriends);
 router.post("/send-message", authMiddleware, userSendMessage);
+router.get("/get-message/:id", authMiddleware, userGetMessage);
 
 module.exports = router;
