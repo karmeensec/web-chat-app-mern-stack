303659b02d99f798a5a90751eb18e3da39f43564
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-05 23:09:43 +0200

    create new div for typing

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -5,32 +5,14 @@ const Message = ({ currentFriend, message, scrollingRef }) => {
   const { userInfo } = useSelector((state) => state.auth);
 
   return (
-    <div className="message-show">
-      {message && message.length > 0
-        ? message.map((m) =>
-            m.senderId === userInfo.id ? (
-              <div className="my-message" ref={scrollingRef}>
-                <div className="image-message">
-                  <div className="my-text">
-                    <p className="message-text">
-                      {m.message.text === "" ? (
-                        <img src={`./images/${m.message.image}`} alt="" />
-                      ) : (
-                        m.message.text
-                      )}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="time">27 August 2023</div>
-              </div>
-            ) : (
-              <div className="friend-message" ref={scrollingRef}>
-                <div className="image-message-time">
-                  <img src={`./images/${currentFriend.image}`} alt="" />
-
-                  <div className="message-time">
-                    <div className="friend-text">
+    <>
+      <div className="message-show">
+        {message && message.length > 0
+          ? message.map((m) =>
+              m.senderId === userInfo.id ? (
+                <div className="my-message" ref={scrollingRef}>
+                  <div className="image-message">
+                    <div className="my-text">
                       <p className="message-text">
                         {m.message.text === "" ? (
                           <img src={`./images/${m.message.image}`} alt="" />
@@ -39,15 +21,49 @@ const Message = ({ currentFriend, message, scrollingRef }) => {
                         )}
                       </p>
                     </div>
+                  </div>
 
-                    <div className="time">26 August 2023</div>
+                  <div className="time">27 August 2023</div>
+                </div>
+              ) : (
+                <div className="friend-message" ref={scrollingRef}>
+                  <div className="image-message-time">
+                    <img src={`./images/${currentFriend.image}`} alt="" />
+
+                    <div className="message-time">
+                      <div className="friend-text">
+                        <p className="message-text">
+                          {m.message.text === "" ? (
+                            <img src={`./images/${m.message.image}`} alt="" />
+                          ) : (
+                            m.message.text
+                          )}
+                        </p>
+                      </div>
+
+                      <div className="time">26 August 2023</div>
+                    </div>
                   </div>
                 </div>
-              </div>
+              )
             )
-          )
-        : ""}
-    </div>
+          : ""}
+      </div>
+
+      <div className="typing-message">
+        <div className="friend-message">
+          <div className="image-message-time">
+            <img src={`./images/${currentFriend.image}`} alt="" />
+
+            <div className="message-time">
+              <div className="friend-text">
+                <p className="time">Typing...</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
