4e04240c801796b7ef75745903c0dcfcd1900c03
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-05 23:15:59 +0200

    props chaining to message component

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -14,6 +14,7 @@ const Chat = ({
   sendEmoji,
   sendImage,
   activeUsers,
+  userTypingMessage,
 }) => {
   return (
     <div className="col-9">
@@ -65,6 +66,7 @@ const Chat = ({
                 currentFriend={currentFriend}
                 message={message}
                 scrollingRef={scrollingRef}
+                userTypingMessage={userTypingMessage}
               />
               <MessageSend
                 handleInputMessageChange={handleInputMessageChange}
