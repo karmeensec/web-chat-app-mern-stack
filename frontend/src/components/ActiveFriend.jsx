89f211d03e6375dfc782bc96f80f24915ce668a4
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-05 17:03:51 +0200

    create handle function to click active friend

--- frontend/src/components/ActiveFriend.jsx
+++ frontend/src/components/ActiveFriend.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
-const ActiveFriend = ({ activeUser }) => {
+const ActiveFriend = ({ activeUser, setCurrentFriend }) => {
+  const handleClickActiveFriend = () => {
+    setCurrentFriend({
+      _id: activeUser.userInfo.id,
+      userName: activeUser.userInfo.userName,
+      email: activeUser.userInfo.email,
+      image: activeUser.userInfo.image,
+    });
+  };
+
   return (
-    <div className="active-friend">
+    <div className="active-friend" onClick={handleClickActiveFriend}>
       <div className="image-active-icon">
         <div className="image">
           <img src={`./images/${activeUser.userInfo.image}`} alt="" />
