e909eb53e585fc62bc1b45391a86704aa3b0c261
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-07 17:43:11 +0200

    routes for seen and delivered message

--- backend/routes/messengerRoute.js
+++ backend/routes/messengerRoute.js
@@ -5,6 +5,8 @@ const {
   userSendMessage,
   userGetMessage,
   userSendImageMessage,
+  userSeenMessage,
+  userDeliveredMessage,
 } = require("../controller/messengerController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
@@ -12,5 +14,7 @@ router.get("/get-friends", authMiddleware, userGetFriends);
 router.post("/send-message", authMiddleware, userSendMessage);
 router.get("/get-message/:id", authMiddleware, userGetMessage);
 router.post("/image-message-send", authMiddleware, userSendImageMessage);
+router.post("/seen-message", authMiddleware, userSeenMessage);
+router.post("/delivered-message", authMiddleware, userDeliveredMessage);
 
 module.exports = router;
