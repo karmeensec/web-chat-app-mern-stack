0a504ff5ba5d77bc0a905ce57d949e6647c45ab6
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-09 13:18:40 +0200

    create route for logout

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 
-const { userRegister, userLogin } = require("../controller/authController");
+const {
+  userRegister,
+  userLogin,
+  userLogout,
+} = require("../controller/authController");
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/user-register", userRegister);
 router.post("/user-login", userLogin);
+router.post("/user-logout", authMiddleware, userLogout);
 
 module.exports = router;
