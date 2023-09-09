dfea3d9a6e7e8090d74d6f25307f2aeb4cd85f79
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-09-09 22:16:50 +0200

    comment redux extension

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -13,7 +13,7 @@ const middleware = [thunkMiddleware];
 export const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
